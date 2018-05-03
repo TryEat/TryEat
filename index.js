b0dec3d20e6fe073322e11d08a706ff50b94c98f
Unknown <questknight@naver.com>
2018-05-04 03:12:34 +0900

    add server routes

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-var dbrouter = require('./routes/db-route.js');
-app.use('/db',dbrouter);
+app.use('/restaurants',require('./routes/server/restaurants'));
+app.use('/follows',require('./routes/server/follows'));
+app.use('/Reviews',require('./routes/server/Reviews'));
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
- 
+
 var port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
---
+++ routes/server/restaurants.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+
+function generateID() {
+    var _id;
+    return _id;
+}
+
+router.get('/', function (req, res) {
+    res.end()
+})
+
+router.get('/:id/byid', function (req, res) {
+    res.end(req.params.id);
+})
+router.get('/:name/byname', function (req, res) {
+    res.end(req.params.name);
+})
+router.get('/:lat/:ion/bylocation', function (req, res) {
+    res.end(req.params.lat +'\n'+ req.params.ion);
+})
+
+router.get('/is_exist/:name/name', function (req, res) {
+    res.end(req.params.name);
+})
+router.get('/is_exist/:lat/:ion/location', function (req, res) {
+    res.end(req.params.lat +'\n'+ req.params.ion);
+})
+
+router.get('/count/:id', function (req, res) {
+    res.end();
+});
+
+router.post('/', function (req, res) {
+    var id = generateID();
+    var information = req.body.information;
+    res.end()
+});
+
+router.put('/', function (req, res) {
+    var id = req.body.id;
+    var information = req.body.information;
+    res.end();
+});
+
+router.delete('/', function (req, res) {
+    var id = req.body.id;
+    res.end();
+});
+
+module.exports = router;
