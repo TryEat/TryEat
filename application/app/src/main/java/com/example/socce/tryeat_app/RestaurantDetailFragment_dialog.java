af6461c63ff09cd615f3b1fa34c8cc85356ac492
ROK <soccer0596@naver.com>
2018-05-11 08:04:57 +0900

    restaurant_add 와 restaurant_datail 을 fragment가 아닌 dialog로 만들고 xml 틀 만들어둠 다음에 할때 review_list_fragment부터 하면됨 근데 fragment 교체가 안됨 다음에 해결해야함

---
+++ application/app/src/main/java/com/example/socce/tryeat_app/RestaurantDetailFragment_dialog.java
@@ -0,0 +1,32 @@
+package com.example.socce.tryeat_app;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by socce on 2018-05-11.
+ */
+
+public class RestaurantDetailFragment_dialog extends Dialog {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WindowManager.LayoutParams lpWindow = new WindowManager.LayoutParams();
+        lpWindow.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        lpWindow.dimAmount = 0.7f;
+        lpWindow.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lpWindow.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        getWindow().setAttributes(lpWindow);
+    }
+
+    public RestaurantDetailFragment_dialog(@NonNull Context context) {
+        super(context);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setCanceledOnTouchOutside(true);
+        setContentView(R.layout.restaurant_detail_fragment);
+    }
+}
